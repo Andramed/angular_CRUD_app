7d9075282cd5c5f9998020a4ce38d1b6c1cbdef9
Vlad Cervac <cervacvad32@gmail.com>
2024-01-18 15:37:18 +0100

    change URL for backe end

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:3000/"
-// export const URL = "http://3.79.245.193:3000/"
+// export const URL = "http://localhost:3000/"
+export const URL = "http://3.79.245.193:3000/"
 // export const  URL_PROD="http://3.79.245.193:3000/"
