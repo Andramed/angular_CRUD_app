47c0073bd21946ae6af8b1b1cf4edecd50c5e606
Vlad Cervac <cervacvad32@gmail.com>
2024-01-25 09:20:01 +0100

    change http endpoint

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,5 +1,5 @@
-export const URL = "http://localhost:3000/"
-// export const URL = "http://3.79.245.193:3000/"
+// export const URL = "http://localhost:3000/"
+export const URL = "http://3.79.245.193:3000/"
 // export const  URL_PROD="http://3.79.245.193:3000/"
 
 export const LOGIN_ROUTE = "signin"
